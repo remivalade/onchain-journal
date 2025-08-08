11cabdf2922d23ec6e3a84a82090cacabdd75f62
remivalade <zeyeepeeman@gmail.com>
2025-08-08 16:23:31 +0200

    projectID

--- web/src/lib/wagmi.ts
+++ web/src/lib/wagmi.ts
@@ -15,7 +15,7 @@ export const queryClient = new QueryClient({
 export const wagmiConfig = getDefaultConfig({
   appName: 'On-Chain Journal',
   // TODO: Replace with your own WalletConnect project ID
-  projectId: 'a8024e8269d39a3209803649ac5d1842',
+  projectId: '6c3b0bc57aeed2aaaa83593adadec525',
   chains: [bob],
   transports: {
     [bob.id]: http(bob.rpcUrls.default.http[0]),
